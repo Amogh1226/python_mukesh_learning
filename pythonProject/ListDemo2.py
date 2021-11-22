cd0b5ea5ad061a0ec4dd951fd05696810ee0dc66
Amogh <amogh.jonnalagadda@gmail.com>
2021-11-22 13:28:40 +0800

    List Programms

--- pythonProject/ListDemo2.py
+++ pythonProject/ListDemo2.py
@@ -1,6 +1,20 @@
 list1=[12,89,78,90,45,34,90,90]
-print(list1[-2])
-
-print(list1[1:4])
+#print(list1[-2])
+#print(list1[1:5])
 
 # print(list1.count(777))
+
+list1[0]=666
+#print(list1)
+
+list1.append("Amogh")
+#print(list1)
+
+list1.append("Python")
+#print(list1)
+
+list1.insert(0,555)
+#print(list1)
+
+list1.insert(2,444)
+print(list1)
---
+++ pythonProject/ListDemo3.py
@@ -0,0 +1,14 @@
+list1=[12,89,78,90,55,44,8,3,2]
+list2=["Amogh","Jonnalagadda","Python"]
+list3="Amogh"
+list1.extend(list2)
+#print(list1)
+list1.extend(list3)
+#print(list1)
+#list2.pop(1)
+
+list2.remove("Amogh")
+print(list2)
+list2.remove("Jonnalagadda")
+#print(list2)
+
